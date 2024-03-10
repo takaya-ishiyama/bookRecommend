7f7f22345f253ef0244c6fa43d5ffbd1e51b1712
takaya-ishiyama <takaya_ishiyama0810@yahoo.co.jp>
2024-03-10 18:06:01 +0900

    a

--- apps/frontend/app/routes/_index.tsx
+++ apps/frontend/app/routes/_index.tsx
@@ -1,38 +1,21 @@
 import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
+  return [{ title: "book recommend" }];
 };
 
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
       <ul>
         <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
+          <a target="_self" href="./login">
+            login
           </a>
         </li>
         <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
+          <a target="_self" href="./app/top">
+            top
           </a>
         </li>
       </ul>
---
+++ apps/frontend/app/routes/app.top.tsx
@@ -0,0 +1,5 @@
+const top = () => {
+  return <div>top</div>;
+};
+
+export default top;
